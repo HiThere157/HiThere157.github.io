9e56688def79182dfc1e1dad05d91dd6d06becfe
David <50298235+HiThere157@users.noreply.github.com>
2021-06-24 17:10:23 +0200

    Add files via upload

---
+++ page.js
@@ -0,0 +1,45 @@
+function click_icon(elem) {
+    window.location.href = list[elem.id];
+}
+
+var list = {"icon0-0": ["Game of Life", "https://www.youtube.com/"], "icon0-1": ["Maze Generator", "https://www.twitch.tv/"]}
+
+function refresh_Icons() {
+    var key = Object.keys(list);
+    for(var i = 0; i < key.length; i++){
+        document.getElementsByName(key[i])[0].innerText = list[key[i]][0];
+    }
+}
+
+function setupIcons() {
+    var main = document.getElementById("overlay_main");
+    for(var i = 0; i < 2; i++){
+        var row = document.createElement("div");
+        row.className = "overlay_rows";
+        row.id = "row" + i.toString();
+
+        if(i != 0){
+            row.style = "padding-top: 25px;";
+        }
+
+        main.appendChild(row);
+
+        for(var j = 0; j < 6; j++){
+            var iconDiv = document.createElement("div");
+            var iconId = "icon" + i.toString() + "-" + j.toString();
+            iconDiv.className = "main_icons";
+            iconDiv.onclick = function() { click_icon(this); };
+            iconDiv.id = iconId;
+
+            var iconP = document.createElement("p");
+            iconP.setAttribute("name", iconId);
+            iconP.style.width = "100px";
+            iconDiv.appendChild(iconP);
+
+            row.appendChild(iconDiv);
+        }
+    }
+    refresh_Icons();
+}
+
+setupIcons();
