b0751b1e3e6cc9a3ad7b5c33218147443e06ee33
David <kdavid03@gmx.de>
2021-09-14 21:50:14 +0200

    v2

--- page.js
+++ page.js
@@ -1,9 +1,9 @@
 function click_icon(elem) {
   let href = list[elem.id][1].split(".");
 
-  if(href[href.length-1] == "html"){
+  if (href[href.length - 1] == "html") {
     window.location.href = href.join(".");
-  }else if(href[href.length-1] == "js"){
+  } else if (href[href.length - 1] == "js") {
     let tmp = document.createElement("script");
     tmp.src = href.join(".");
 
@@ -11,50 +11,38 @@ function click_icon(elem) {
   }
 }
 
-var list = { "icon0-0": ["Game of Life", "/4gol/game_of_life.html"], "icon0-1": ["Maze Generator", "/6maze/maze.html"], "icon0-2": ["Mine Sweeper", "/9mineSweeper/mineSweeper.html"], "icon0-3": ["Ray Cast", "/11rayCast/ray.html"],"icon1-0": ["HTML Shooter", "shooter/shooter.js"] ,"icon1-5": ["Data Viewer v2", "/10data_viewer_v2/data_viewer.html"] }
+var list = { "icon0": ["Game of Life", "/4gol/game_of_life.html"], "icon1": ["Maze Generator", "/6maze/maze.html"], "icon2": ["Mine Sweeper", "/9mineSweeper/mineSweeper.html"], "icon3": ["Ray Cast", "/11rayCast/ray.html"], "icon4": ["HTML Shooter", "shooter/shooter.js"], "icon13": ["DataViewer v2", "/10data_viewer_v2/data_viewer.html"] }
+var key = Object.keys(list);
 
 function refresh_Icons() {
-  var key = Object.keys(list);
   for (var i = 0; i < key.length; i++) {
     document.getElementsByName(key[i])[0].innerText = list[key[i]][0];
-    document.getElementsByName(key[i] + "_i")[0].innerText = parseInt(key[i][4] * 6) + parseInt(key[i][6]);
+    document.getElementsByName(key[i] + "_i")[0].innerText = key[i].substring(4,key[i].length);
   }
 }
 
 function setupIcons() {
   var main = document.getElementById("overlay_main");
-  for (var i = 0; i < 2; i++) {
-    var row = document.createElement("div");
-    row.className = "overlay_rows";
-    row.id = "row" + i.toString();
+  for (var j = 0; j < 14; j++) {
+    var iconDiv = document.createElement("div");
+    var iconId = "icon" + j.toString();
+    iconDiv.className = "main_icons";
+    iconDiv.onclick = function () { click_icon(this); };
+    iconDiv.id = iconId;
 
-    if (i != 0) {
-      row.style = "padding-top: 25px;";
-    }
+    var iconI = document.createElement("p");
+    iconI.setAttribute("name", iconId.toString() + "_i");
+    iconI.className = "main_i";
+    iconDiv.appendChild(iconI);
 
-    main.appendChild(row);
+    var iconP = document.createElement("p");
+    iconP.setAttribute("name", iconId);
+    iconP.className = "main_p";
+    iconDiv.appendChild(iconP);
 
-    for (var j = 0; j < 6; j++) {
-      var iconDiv = document.createElement("div");
-      var iconId = "icon" + i.toString() + "-" + j.toString();
-      iconDiv.className = "main_icons";
-      iconDiv.onclick = function () { click_icon(this); };
-      iconDiv.id = iconId;
-
-      var iconI = document.createElement("p");
-      iconI.setAttribute("name", iconId.toString() + "_i");
-      iconI.className = "main_i";
-      iconDiv.appendChild(iconI);
-
-      var iconP = document.createElement("p");
-      iconP.setAttribute("name", iconId);
-      iconP.className = "main_p";
-      iconDiv.appendChild(iconP);
-
-      row.appendChild(iconDiv);
-    }
+    main.appendChild(iconDiv);
   }
+
   refresh_Icons();
 }
-
 setupIcons();
