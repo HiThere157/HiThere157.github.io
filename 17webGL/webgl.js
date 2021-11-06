81b07206ffb60d8d98ed29315a7deb9fd6c0d928
David <kdavid03@gmx.de>
2021-11-06 19:25:09 +0100

    fix

--- 17webGL/webgl.js
+++ 17webGL/webgl.js
@@ -2,34 +2,35 @@ document.body.onload = init;
 var obj;
 
 var settings = {
-  "castle": { cam: [-30, 50, 60], scale: [0.5, 0.5, 0.5], pos: [0, -20, 0], shadowD: 100, selfShadow: true, credits: '"stylised sky player home dioroma" (<u>https://skfb.ly/P6nF</u>) by Sander Vander Meiren is licensed under Creative Commons Attribution (<u>http://creativecommons.org/licenses/by/4.0/</u>).' },
-  "gun": { cam: [-20, 15, 120], scale: [0.15, 0.15, 0.15], pos: [0, -50, 0], shadowD: 300, selfShadow: false, credits: '"RAINIER AK - 3D" (<u>https://skfb.ly/6ynGy</u>) by skartemka is licensed under Creative Commons Attribution-NonCommercial (<u>http://creativecommons.org/licenses/by-nc/4.0/</u>).' }
+  "castle": { cam: [-30, 50, 60], scale: [0.5, 0.5, 0.5], pos: [0, -20, 0], shadowD: 100, selfShadow: true, fog: true, zoom: [10, 400], credits: '"stylised sky player home dioroma" (<u>https://skfb.ly/P6nF</u>) by Sander Vander Meiren is licensed under Creative Commons Attribution (<u>http://creativecommons.org/licenses/by/4.0/</u>).' },
+  "gun": { cam: [-20, 15, 120], scale: [0.15, 0.15, 0.15], pos: [0, -50, 0], shadowD: 300, selfShadow: false, fog: false, zoom: [20, 400], credits: '"RAINIER AK - 3D" (<u>https://skfb.ly/6ynGy</u>) by skartemka is licensed under Creative Commons Attribution-NonCommercial (<u>http://creativecommons.org/licenses/by-nc/4.0/</u>).' }
 }
 
 var modelName = window.location.search.substr(1) || "castle";
 
 function init() {
-  if(settings[modelName] == undefined){
+  if (settings[modelName] == undefined) {
     return 0;
   }
-  
+
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30000);
   const gltfLoader = new THREE.GLTFLoader();
-  
+
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(0x2785c4, 0.5);
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   document.body.appendChild(renderer.domElement);
-  
+
+  //load gltf model
   gltfLoader.load("assets/" + modelName + "/scene.gltf", function (gltf) {
     document.getElementById("credits").innerHTML = settings[modelName].credits;
     model = gltf.scene;
     model.scale.set(...settings[modelName].scale);
     model.position.set(...settings[modelName].pos);
-    
+
     if (settings[modelName].selfShadow) {
       model.traverse(function (node) {
         if (node.isMesh) {
@@ -42,11 +43,14 @@ function init() {
     scene.add(gltf.scene);
   });
 
+  //add lights
   const light = getDLight(-70, 200, 250, settings[modelName].shadowD);
   scene.add(light);
   const Alight = getALight(0.7);
   scene.add(Alight);
+  if (settings[modelName].fog) { scene.fog = new THREE.Fog(0x2785c4) };
 
+  //set camera
   camera.position.set(...settings[modelName].cam);
   camera.lookAt(0, 0, 0);
 
@@ -75,6 +79,9 @@ function init() {
     hA: helpAxes = () => {
       this.helperAxesVis = !this.helperAxesVis;
       scene.getObjectByName("axesHelper").visible = this.helperAxesVis;
+    },
+    printCam: printCamPos = () => {
+      alert([...camera.position].map(val => { return Number(val.toFixed(3)) }).join(", "));
     }
   }
   const lightFolder = gui.addFolder("Light");
@@ -92,10 +99,13 @@ function init() {
   const helperFolder = gui.addFolder("Helpers");
   helperFolder.add(obj, "hS").name("Toogle Shadow Helper")
   helperFolder.add(obj, "hA").name("Toogle Axes Helper")
-  helperFolder.open()
+
+  const cameraFolder = gui.addFolder("Camera");
+  cameraFolder.add(obj, "printCam").name("Print Camera Position");
 
   //rederer & controls
   const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  [controls.minDistance, controls.maxDistance] = settings[modelName].zoom;
   const render = () => {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
