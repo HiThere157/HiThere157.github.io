2b0219000518c69fbb09b89428d3f5fdf18dba0d
David <kdavid03@gmx.de>
2022-04-13 22:05:13 +0200

    background

---
+++ background.js
@@ -0,0 +1,79 @@
+class Particle {
+  static canvas = document.getElementById("canvas");
+  static context = Particle.canvas.getContext("2d");
+  static init() {
+    Particle.canvas.width = window.innerWidth;
+    Particle.canvas.height = window.innerHeight;
+    Particle.canvas.style = "position: absolute; top:0;";
+
+    Particle.context.strokeStyle = "#fff";
+    Particle.context.lineWidth = .1;
+    Particle.context.fillStyle = "#fff";
+
+    const draw = () => {
+      Particle.context.clearRect(0, 0, canvas.width, canvas.height);
+      Particle.particles.forEach(particle => {
+        particle.update();
+      });
+    };
+    
+    setInterval(draw, 33);
+    Particle.generateParticles();
+  }
+
+  static particles = [];
+  static generateParticles() {
+    Particle.particles = [];
+    for (let i = 0; i < 100; i++) {
+      Particle.particles.push(new Particle(Math.random() * canvas.width, Math.random() * canvas.height));
+    }
+  }
+
+  static particlesInRadius(x, y, radius2) {
+    return Particle.particles.filter(particle => {
+      const distance2 = Math.pow(particle.x - x, 2) + Math.pow(particle.y - y, 2);
+      return distance2 < radius2;
+    });
+  }
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = Math.random() * 2 + 1;
+
+    this.velocity = {
+      x: Math.random() * 1 - .5,
+      y: Math.random() * 1 - .5
+    };
+  }
+
+  update() {
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    if (this.x > canvas.width) {
+      this.x = 0;
+    } else if (this.x < 0) {
+      this.x = canvas.width;
+    }
+
+    if (this.y > canvas.height) {
+      this.y = 0;
+    } else if (this.y < 0) {
+      this.y = canvas.height;
+    }
+
+    Particle.particlesInRadius(this.x, this.y, 27500).forEach(particle => {
+      Particle.context.beginPath();
+      Particle.context.moveTo(this.x, this.y);
+      Particle.context.lineTo(particle.x, particle.y);
+      Particle.context.stroke();
+    });
+
+    Particle.context.beginPath();
+    Particle.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    Particle.context.fill();
+  }
+}
+
+document.onload = Particle.init();
